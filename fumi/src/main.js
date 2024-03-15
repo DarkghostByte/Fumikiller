fdb8b9bb0497ff4db0d50d2243c82163b9b5e498
DarkGhostByte <Liadeochavez@gmail.com>
2024-03-15 16:37:21 -0600

    Imagen del login
    Se inserto la imagen del login, el .env del vue, se implemento axios

--- fumi/src/main.js
+++ fumi/src/main.js
@@ -4,8 +4,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles.css'
 import router from './routes/'
+import axios from  "axios"
+axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
 
 createApp(App)
     .use(ElementPlus)
     .use(router)
     .mount('#app')
+
--- fumi/src/views/auth/LoginComponent.vue
+++ fumi/src/views/auth/LoginComponent.vue
@@ -3,6 +3,7 @@
     <div class="flex justify-center items-center h-screen bg-neutral-950">
         <div class="w-1/2">
             <div class="DivAzul">
+                <img v-bind:src="url+'img/logofk.png'" class="imgFk">               
                 <div class="DivNaranja">
                     <h1 style="margin-top:15%;font-size: 60px; ">FUMI-KILLE'R</h1>
                     <button class="btnInicio">INICIO</button>
@@ -54,10 +55,11 @@
     margin-top: 15%;
     background-color: black; 
     border-radius: 45px; 
-    width:240px; 
-    height:80px; 
+    width:220px; 
+    height:75px; 
     color:aliceblue; 
-    font-size:50px; 
+    font-size:40px; 
+    font-family: Arial, Helvetica, sans-serif;
     text-align: center;
     border: none;    
     transition: 2s;
@@ -71,10 +73,20 @@
     width: 245px;
     border: 1px,solid,black;
 }
+.imgFk{
+    width:280px; 
+    height:470px; 
+    position:absolute;
+    margin-top: 5%;
+}
 </style>
 
 <script>
     export default{
         name:'LoginComponent',
+        data:()=>({
+            url:process.env.VUE_APP_ROOT_ASSETS,
+        }),
     }
+        
 </script>
