51a68cc002a8b697a00ef59f78ceea255fa36566
JoseChuyPepe28 <lm8555555@gmail.com>
2024-03-24 15:37:22 -0600

    Diseño pantallas Clientes y Ordenes de trabajo
    Primer diseño de las pantallas de Ordenes de trabajo y Clientes

--- fumi/src/views/admin/clients/AdminClientsComponent.vue
+++ fumi/src/views/admin/clients/AdminClientsComponent.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
-            <div class="mr-6">
-              <h1 class="text-4xl font-semibold mb-2">Clientes</h1>
-            </div>
-            <div class="flex flex-wrap items-start justify-end -mb-3">
-                <button class="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
-                    Add User
-                  </button>
-            </div>
+  <!-- Importar Iconos-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <div>
+    <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-start">
+        <div class="mr-6">
+          <h1 class="py-10 px-5 text-4xl font-semibold mb-2">Clientes</h1>
         </div>
+        <div class="flex flex-wrap items-start justify-end -mb-3">
+          <button class="inline-flex px-5 py-3 text-white bg-blue-400 hover:bg-blue-700 focus:bg-blue-800 rounded-md ml-6 mb-3" style="color:black">
+            <i class="fa fa-search" aria-hidden="true" style="margin-top: 5px;
+            margin-left: -5px; margin-right:10px;"></i>                
+            Consultar Cliente
+          </button>
+          <button class="inline-flex px-5 py-3 text-white bg-green-400 hover:bg-green-600 focus:bg-green-700 rounded-md ml-6 mb-3" style="color:black">
+            <i class="fa fa-plus-circle" aria-hidden="true" style="margin-top: 5px;
+            margin-left: -5px; margin-right:10px;"></i>                
+            Nuevo Cliente
+          </button>
+          <button class="inline-flex px-5 py-3 text-white bg-yellow-400 hover:bg-yellow-500 focus:bg-yellow-600 rounded-md ml-6 mb-3" style="color:black">
+            <i class="fa fa-pencil" aria-hidden="true" style="margin-top: 5px;
+            margin-left: -5px; margin-right:10px;"></i>                
+            Editar Cliente
+          </button>
+        </div>
+    </div>
         <!-- TABLE DATA -->
         <div class="flex">
             <el-table
@@ -17,17 +31,20 @@
                 :default-sort="{ prop: 'name', order: 'descending' }"
                 style="width: 100%"
             >
-                <el-table-column prop="date" label="Date" sortable width="180"  />
-                <el-table-column prop="name" label="Name"  sortable />
-                <el-table-column prop="address" label="Address" :formatter="formatter" />
+                <el-table-column prop="apCliente" label="Apellido Paterno" sortable width="180"  />
+                <el-table-column prop="amCliente" label="Apellido Materno"  sortable />
+                <el-table-column prop="nameCliente" label="Nombres"  sortable />
+                <el-table-column prop="address" label="Dirección" :formatter="formatter" />
+                <el-table-column prop="numCelu" label="Numero Celular"  sortable />
+                <el-table-column prop="numTel" label="Numero Fijo"  sortable />
+                
                 <el-table-column label="">
                     <template #default>
-                      <el-button size="small" @click="handleEdit()">Edit</el-button>
-                      <el-button
+                      <el-button style="color:black"
                         size="small"
                         type="danger"
                         @click="handleDelete()"
-                        >Delete</el-button
+                        ><i class="fa fa-trash" aria-hidden="true"></i></el-button
                       >
                     </template>
                   </el-table-column>
@@ -39,7 +56,7 @@
   </template>
   
   <script>
-     
+    
       export default {
           name:'AdminHomeComponent',
           
@@ -48,24 +65,36 @@
           }),
           mounted(){
             this.tableData = [{
-                    date: '2016-05-03',
-                    name: 'Tom',
+                    apCliente: 'Peña',
+                    amCliente:'Mora',
+                    nameCliente: 'Luis Angel',
                     address: 'No. 189, Grove St, Los Angeles',
+                    numCelu:'6361306641',
+                    numTel:'636130664',
                 },
                 {
-                    date: '2016-05-02',
-                    name: 'Tom',
+                    apCliente: 'Chavez',
+                    amCliente:'Chavez',
+                    nameCliente: 'Jesus Liadeo',
                     address: 'No. 189, Grove St, Los Angeles',
+                    numCelu:'636125070',
+                    numTel:'636125070',
                 },
                 {
-                    date: '2016-05-04',
-                    name: 'Tom',
+                    apCliente: 'Mozqueda',
+                    amCliente:'Gomez',
+                    nameCliente: 'Emiliano',
                     address: 'No. 189, Grove St, Los Angeles',
+                    numCelu:'6361224988',
+                    numTel:'6361224988',
                 },
                 {
-                    date: '2016-05-01',
-                    name: 'Tom',
+                    apCliente: 'Madrid',
+                    amCliente:'Holguin',
+                    nameCliente: 'Alan Saul',
                     address: 'No. 189, Grove St, Los Angeles',
+                    numCelu:'6361024108',
+                    numTel:'6361024108',
                 },
             ]
           },
--- fumi/src/views/admin/works/AdminWorksComponent.vue
+++ fumi/src/views/admin/works/AdminWorksComponent.vue
@@ -1,13 +1,27 @@
 <template>
+  <!-- Importar Iconos-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <div>
-        <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
+        <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-start" >
             <div class="mr-6">
-              <h1 class="text-4xl font-semibold mb-2">Orden de trabajo</h1>
+              <h1 class="py-10 px-5 text-4xl font-semibold mb-2">Ordenes de trabajo</h1>
             </div>
             <div class="flex flex-wrap items-start justify-end -mb-3">
-                <button class="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
-                    Add User
-                  </button>
+              <button class="inline-flex px-5 py-3 text-white bg-blue-400 hover:bg-blue-700 focus:bg-blue-800 rounded-md ml-6 mb-3" style="color:black">
+                <i class="fa fa-search" aria-hidden="true" style="margin-top: 5px;
+                margin-left: -5px; margin-right:10px;"></i>                
+                Consultar Orden
+              </button>
+              <button class="inline-flex px-5 py-3 text-white bg-green-400 hover:bg-green-600 focus:bg-green-700 rounded-md ml-6 mb-3" style="color:black">
+                <i class="fa fa-plus-circle" aria-hidden="true" style="margin-top: 5px;
+                margin-left: -5px; margin-right:10px;"></i>                
+                Nueva Orden
+              </button>
+              <button class="inline-flex px-5 py-3 text-white bg-yellow-400 hover:bg-yellow-500 focus:bg-yellow-600 rounded-md ml-6 mb-3" style="color:black">
+                <i class="fa fa-pencil" aria-hidden="true" style="margin-top: 5px;
+                margin-left: -5px; margin-right:10px;"></i>                
+                Editar Orden
+              </button>
             </div>
         </div>
         <!-- TABLE DATA -->
@@ -17,17 +31,19 @@
                 :default-sort="{ prop: 'name', order: 'descending' }"
                 style="width: 100%"
             >
-                <el-table-column prop="date" label="Date" sortable width="180"  />
-                <el-table-column prop="name" label="Name"  sortable />
-                <el-table-column prop="address" label="Address" :formatter="formatter" />
+                <el-table-column prop="nameCliente" label="Nombre del Cliente"  sortable />
+                <el-table-column prop="nameComercial" label="Nombre Comercial"  sortable />
+                <el-table-column prop="fumi" label="Fumigador"  sortable />
+                <el-table-column prop="direccionCliente" label="Dirección" :formatter="formatter" />
+                <el-table-column prop="fechaOrden" label="Fecha de Orden" sortable width="180"  />
+                <el-table-column prop="fechaAsistencia" label="Fecha de asistencia" sortable width="180"  />
                 <el-table-column label="">
                     <template #default>
-                      <el-button size="small" @click="handleEdit()">Edit</el-button>
-                      <el-button
+                      <el-button style="color:black;"
                         size="small"
                         type="danger"
                         @click="handleDelete()"
-                        >Delete</el-button
+                        ><i class="fa fa-trash" aria-hidden="true"></i></el-button
                       >
                     </template>
                   </el-table-column>
@@ -39,7 +55,7 @@
   </template>
   
   <script>
-     
+  
       export default {
           name:'AdminHomeComponent',
           
@@ -48,24 +64,36 @@
           }),
           mounted(){
             this.tableData = [{
-                    date: '2016-05-03',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles',
+                    nameCliente:'Luis Angel Peña Mora',
+                    nameComercial:"La Pepa",
+                    fumi:"Juan Carlos",
+                    direccionCliente:'No. 189, Grove St, Los Angeles',
+                    fechaOrden: '2024-03-24',
+                    fechaAsistencia: '2024-03-24',
                 },
                 {
-                    date: '2016-05-02',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles',
+                    nameCliente:'Luis Angel Peña Mora',
+                    nameComercial:"La Pepa",
+                    fumi:"Juan Carlos",
+                    direccionCliente:'No. 189, Grove St, Los Angeles',
+                    fechaOrden: '2024-03-24',
+                    fechaAsistencia: '2024-03-24',
                 },
                 {
-                    date: '2016-05-04',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles',
+                    nameCliente:'Luis Angel Peña Mora',
+                    nameComercial:"La Pepa",
+                    fumi:"Juan Carlos",
+                    direccionCliente:'No. 189, Grove St, Los Angeles',
+                    fechaOrden: '2024-03-24',
+                    fechaAsistencia: '2024-03-24',
                 },
                 {
-                    date: '2016-05-01',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles',
+                    nameCliente:'Luis Angel Peña Mora',
+                    nameComercial:"La Pepa",
+                    fumi:"Juan Carlos",
+                    direccionCliente:'No. 189, Grove St, Los Angeles',
+                    fechaOrden: '2024-03-24',
+                    fechaAsistencia: '2024-03-24',
                 },
             ]
           },
