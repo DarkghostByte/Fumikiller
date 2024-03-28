f32c4895ff8c2f2be8e2f6ded6ce4f0a05beee72
JoseChuyPepe28 <lm8555555@gmail.com>
2024-03-28 12:49:34 -0600

    Correcciones
    Correcciones de diseño

--- fumi/src/views/admin/home/AdminHomeComponent.vue
+++ fumi/src/views/admin/home/AdminHomeComponent.vue
@@ -1,5 +1,9 @@
 <template>
+    <!-- Importar Iconos-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <div class="">
+    <i class="fa fa-file-pdf-o" aria-hidden="true" style="color:black;"></i>
+
     <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
       <div class="mr-6">
         <h1 class="text-4xl font-semibold mb-2">Dashboard</h1>
