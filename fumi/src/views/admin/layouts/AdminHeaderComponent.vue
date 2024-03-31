6646e80b2cdd258ef4d65e94c85804b07910bf92
DarkGhostByte <Liadeochavez@gmail.com>
2024-03-30 19:56:16 -0600

    Modificacion en el header
    Se quito el header de las pantallas (Ya no se visualiza)

--- fumi/src/views/admin/layouts/AdminHeaderComponent.vue
+++ fumi/src/views/admin/layouts/AdminHeaderComponent.vue
@@ -1,4 +1,9 @@
 <template>
+  <div>
+    
+  </div>
+  <!--
+
     <header class="flex items-center h-20 px-6 sm:px-10 bg-white">
         <button class="block sm:hidden relative flex-shrink-0 p-2 mr-2 text-gray-600 hover:bg-gray-100 hover:text-gray-800 focus:bg-gray-100 focus:text-gray-800 rounded-full">
           <span class="sr-only">Menu</span>
@@ -23,6 +28,9 @@
           </div>
         </div>
       </header>
+
+  -->
+    
 </template>
 <script>
     export default{
--- fumi/src/views/admin/layouts/AdminLayoutComponent.vue
+++ fumi/src/views/admin/layouts/AdminLayoutComponent.vue
@@ -2,7 +2,7 @@
     <div class="flex bg-gray-100 min-h-screen">
        <AdminAsideComponent />
        <div class="flex-grow text-gray-800">
-         <AdminHeaderComponent />
+        <!--  <AdminHeaderComponent /> -->
          <main class="p-6 sm:p-10 space-y-6">
             <router-view></router-view>
          </main>
@@ -14,13 +14,13 @@
  
  <script>
      import AdminAsideComponent from '@/views/admin/layouts/AdminAsideComponent.vue'
-     import AdminHeaderComponent from '@/views/admin/layouts/AdminHeaderComponent.vue'
+     //import AdminHeaderComponent from '@/views/admin/layouts/AdminHeaderComponent.vue'
      import AdminFooterComponent from '@/views/admin/layouts/AdminFooterComponent.vue'
      export default {
          name:'AdminLayoutComponent',
          components:{
            AdminAsideComponent,
-           AdminHeaderComponent,
+           //AdminHeaderComponent,
            AdminFooterComponent
          }
      }
