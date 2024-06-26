3ee4b9cfbfa8f5164233e1daf447440c4d21d4bb
JoseChuyPepe28 <lm8555555@gmail.com>
2024-06-26 12:37:33 -0600

    Diseño Admin
    Se diseño la pantalla de Administración

--- fumi/src/views/admin/admin/AdminAdminComponent.vue
+++ fumi/src/views/admin/admin/AdminAdminComponent.vue
@@ -6,20 +6,63 @@
       <h1 class="py-10 px-5 text-4xl font-semibold mb-2">Administración</h1>
     </div>
 
-    <div class="flex flex-wrap items-start justify-start">
-      <router-link to="/admin/clients"
-      class="inline-flex px-5 py-3 text-white bg-blue-400 hover:bg-blue-700 focus:bg-blue-800 rounded-md ml-6 mb-3"
-          style="color:black; width:300px; height:100px; font-size:15px;">
-            <i class="fa fa-search" aria-hidden="true" style="margin-top: 5px;
-            margin-left: -5px; margin-right:10px;"></i>                
-            Consultar Cliente
+    <div  class="flex flex-wrap items-start justify-start">
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-blue-400 hover:bg-blue-700 focus:bg-blue-800 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px; text-align: center; ;">
+            <i class="fa-solid fa-city" aria-hidden="true" style="margin-top: 10%;
+            margin-left: -5px; margin-right:10px; font-size:20px;"></i>                
+            <h2>Ciudades</h2>
           </router-link>
 
           <router-link to="/admin/clients/agregarComercio-clients"
-          class="inline-flex px-5 py-3 text-white bg-green-400 hover:bg-green-600 focus:bg-green-700 rounded-md ml-6 mb-3" style="color:black; width:300px; height:100px; font-size:30px;">
-            <i class="fa-solid fa-building" aria-hidden="true" style="margin-top: 5px; 
-            margin-left: -5px; margin-right:10px;"></i>                
-            Colonias
+          class="inline-flex px-5 py-3 text-white bg-green-400 hover:bg-green-600 focus:bg-green-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+            <i class="fa-solid fa-house" aria-hidden="true" style="margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Colonias</h2>
+          </router-link>
+
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-orange-400 hover:bg-orange-600 focus:bg-orange-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+          <i class="fa-solid fa-apartment"></i>
+            <i class="fa-solid fa-school-flag" aria-hidden="true" style="height:15px; margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Asentamientos</h2>
+          </router-link>
+
+
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-purple-400 hover:bg-purple-600 focus:bg-purple-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+            <i class="fa-regular fa-address-card" aria-hidden="true" style="margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Empleados</h2>
+          </router-link>
+
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-blue-400 hover:bg-blue-600 focus:bg-blue-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+            <i class="fa-solid fa-bug" aria-hidden="true" style="margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Problematicas</h2>
+          </router-link>
+
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-green-400 hover:bg-green-600 focus:bg-green-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+            <i class="fa-solid fa-house-chimney-user" aria-hidden="true" style="margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Productos Internos</h2>
+          </router-link>
+
+          <router-link to="/admin/clients/agregarComercio-clients"
+          class="inline-flex px-5 py-3 text-white bg-orange-400 hover:bg-orange-600 focus:bg-orange-700 rounded-md ml-6 mb-3" 
+          style="color:black; width:300px; height:100px; font-size:20px;">
+            <i class="fa-solid fa-house-circle-xmark" aria-hidden="true" style="margin-top: 10%; 
+            margin-left: -5px; margin-right:10px; "></i>                
+            <h2>Productos Externos</h2>
           </router-link>
     </div>
   </div>
@@ -77,3 +120,16 @@
           }
       }
   </script>
+
+  <style>
+  h2{
+    margin-top: 8%;
+    text-align: center;
+  }
+.box-item{
+  position: absolute;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+</style>
