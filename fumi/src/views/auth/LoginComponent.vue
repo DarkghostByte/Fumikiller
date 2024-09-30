aed69d4fde22f82f0584be9bcf4bd3edd576737e
DarkGhostByte <Liadeochavez@gmail.com>
2024-09-30 10:36:58 -0600

    Modificacion del update

--- fumi/src/views/auth/LoginComponent.vue
+++ fumi/src/views/auth/LoginComponent.vue
@@ -1,5 +1,27 @@
 <template>
-    
+    <div>
+        <div class="flex justify-center items-center h-screen bg-gradient-to-r from-orange-500 from-25% to-blue-800 to-60%">
+            <div class="flex" style="width:60%; height: 80%;">
+                <div class="contenedor1 flex justify-center items-center">
+                    <img v-bind:src="url+'img/logofk.png'" class="imgFk">               
+                </div>
+                <div class="contenedor2 row justify-center items-center">
+                    <div>
+                        <h1 class="text-center text-8xl font-bold py-10 text-[#000000]">FUMI-KILLE'R</h1>
+                    </div>
+                    <div class="flex justify-center items-center">
+                        <router-link to="/admin/home">
+                            <button class="btnInicio">
+                                Inicio
+                            </button>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!--
     
     <div class="flex justify-center items-center h-screen bg-gradient-to-r from-orange-500 from-25% to-blue-800 to-60%">
         <div class="w-1/2">
@@ -26,7 +48,7 @@
 
 
 
-<!--
+
 
     <div class="w-10 h-10" style="background-color: black;">
 
@@ -42,59 +64,69 @@
 </template>
 
 <style>
-.DivAzul{
-    justify-content: center;
-    margin-left: -10%; 
-    margin-top: 2%; 
-    border-radius: 25px;
-    width: 100%; height:90vh; 
-    background-color:#0F0F88; 
-}
-.DivNaranja{
-    position:absolute; 
-    text-align: center; 
-    margin-left: 20%;  
-    border-radius: 25px; 
-    width: 40%; height:90vh; 
-    background-color:#FD5902; 
 
-}
-.btnInicio{
+.btnInicio {
     margin-top: 10%;
-    background-color: black; 
-    border-radius: 50px; 
-    width:270px; 
-    height:90px; 
-    color:aliceblue; 
-    font-size:40px; 
+    background-color: black;
+    border-radius: 50px;
+    width: 270px;
+    height: 90px;
+    color: aliceblue;
+    font-size: 40px;
     text-align: center;
-    border: none;    
+    border: none;
     transition: .5s;
 }
-.btnInicio:hover{
-    background-color:#0F0F88;
+
+.btnInicio:hover {
     align-items: center;
     transform: scale(1.1);
     text-align: center;
-    height: 85px;
-    width: 245px;
-    border: 1px,solid,black;
+    height: 95px;
+    width: 280px;
+    border: 1px solid black; 
+    /*background: linear-gradient(to right, white, black);*/
+    color: white;
+    font-weight: bolder; 
 }
-.imgFk{
-    width:260px; 
-    height:450px; 
-    position:absolute;
-    margin-top: 5%;
-    margin-left: .7%;
+
+
+
+
+
+.contenedor1 {
+    background-color: #1e40af;
+    width: 45%;
+    height: 100%;
+    border-radius: 25px 0% 0% 25px;
 }
+
+.contenedor2 {
+    background-color: #f97316;
+    width: 55%;
+    height: 100%;
+    border-radius: 0% 25px 25px 0%;
+}
+
+.imgFk {
+    width: 20%;
+    height: 70%;
+    position: absolute;
+}
+
+h1:hover {
+    transform: scale(1.1);
+    text-align: center;
+  }
+
 </style>
 
 <script>
-    export default{
-        name:'LoginComponent',
-        data:()=>({
-            url:process.env.VUE_APP_ROOT_ASSETS,
-        }),
-    }
-        
+export default {
+    name: 'LoginComponent',
+    data: () => ({
+        url: process.env.VUE_APP_ROOT_ASSETS,
+    }),
+}
+
 </script>
